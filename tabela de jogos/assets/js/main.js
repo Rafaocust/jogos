afdd9478fad34e0a699ef2691e8ce55e6a278b6c
Rafaocust <rcustodio638@gmail.com>
2024-12-11 21:47:59 -0300

    Add files via upload

---
+++ tabela de jogos/assets/js/main.js
@@ -0,0 +1,36 @@
+function createGame(player1, hour, player2) {
+    return `
+        <li>
+            <img src="assets/times/icon-${player1}.png"alt="Bandeira do ${player1}">
+            <strong>${hour}</strong>
+            <img src="assets/times/icon-${player2}.png"alt="Bandeira do ${player2}">
+        </li>
+    `
+}
+function createCard(date, day, games) {
+    return `
+        <div class="card">
+            <h2>${date} <span>${day}</span></h2>
+            <ul>
+                ${games}
+            </ul>
+        </div>
+    `
+}
+
+document.getElementById("app").innerHTML = `
+<main id="cards">
+${createCard("11/12", "Quarta Feira",
+     createGame("atletico", "19:00", "bota"))}
+     ${createCard("15/12", "domingo",
+        createGame("flamengo", "16:00", "palmeiras"))}
+         ${createCard("15/12", "domingo",
+        createGame("corinthians", "16:00", "vasco"))}
+         ${createCard("15/12", "domingo",
+        createGame("bahia", "16:00", "sp"))}
+   
+   
+
+
+<main/>
+`;
